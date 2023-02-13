32fbb6110469ab3d5115be8c5ab6eefb9bb9f2aa
Stephen Hutt <stephen.hutt@me.com>
2023-02-13 11:09:40 -0700

    Lab 16 In Class Code

---
+++ Lab16/lab16strings.py
@@ -0,0 +1,14 @@
+"""
+Author: Stephen Hutt
+Course: 1351
+Content: In Class Demo
+"""
+
+myString = "Hello World"
+
+print(len(myString))
+# for i in range(len(myString)):
+#     #Print 1 character
+#     print(myString[i])
+
+print(myString.index("l"))
