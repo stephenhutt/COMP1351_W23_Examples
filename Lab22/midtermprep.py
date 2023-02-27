e1eebe39e0ac9ed6ef7c11b7ab0d925940190237
Stephen Hutt <stephen.hutt@me.com>
2023-02-27 16:39:15 -0700

    Comments update

--- Lab22/midtermprep.py
+++ Lab22/midtermprep.py
@@ -1,7 +1,4 @@
-
-
-
-
+# Question 3
 for i in range(3):
     for j in range(7):
         #print("OX", end="")
@@ -11,28 +8,34 @@ for i in range(3):
             print("X", end="")
     print()
 
-
+# Question 4
 sentence = "The Quick brown fox jumps over the lazy dog"
 
+#Print T
 print(sentence[0])
-
+#Print quick - not lower case q
 quickVar = sentence[4:9]
 quickVar = quickVar.lower()
 
 print(quickVar)
 
+#Print backwards 
 print(sentence[-1:-9:-1])
 
 
 
+#Question 1
 numbers = [1.23, 2.14, 5.34, 3.01]
 for num in numbers:
     if num > 2:
         print(num)
 
-
+#Question 7
 password = input("Enter Password: ")
-while not password.lower() == "open sesame":
+password = password.lower()
+while not password == "open sesame":
     print("Nope")
     password = input("Enter Password: ")
+    password = password.lower()
+    print(password)
 print("Congrats")
